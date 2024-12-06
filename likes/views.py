2d8da4ebb0ddee8602c95fc99efe7f098da271d0
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-12-06 21:21:54 +0000

    updated the post views and serializers to allow image sizes

--- likes/views.py
+++ likes/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 from profiles.permissions import IsOwnerOrReadOnly
 from .models import Like
 from .serializers import LikeSerializer
@@ -10,4 +12,13 @@ class LikeList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
+        # Check if the like already exists
+        if Like.objects.filter(owner=self.request.user, post=serializer.validated_data['post']).exists():
+            raise ValidationError("You have already liked this post.")
         serializer.save(owner=self.request.user)
+
+# Retrieve and delete likes
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [IsOwnerOrReadOnly]
