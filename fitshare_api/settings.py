4ad5ff46af598040b4f02a6095f8af4a6e91267b
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-12-06 07:32:42 +0000

    create post model

--- fitshare_api/settings.py
+++ fitshare_api/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'profiles',
+    'posts',
     'rest_framework',
 ]
 
---
+++ posts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from profiles.models import Profile
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(upload_to='post_images/', blank=True)
+
+    def __str__(self):
+        return self.title
