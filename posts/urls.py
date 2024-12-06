5b32d23453b03ccd6f4f738e838042b40992bb26
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-12-06 09:56:48 +0000

    updated the urls on post model

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from .views import PostList, PostDetail
+from django.urls import path, include
+from posts.views import PostList, PostDetail
 
 urlpatterns = [
-    path('', PostList.as_view(), name='post-list'),
-    path('<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    path('posts/', PostList.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    path('profiles/', include('profiles.urls')),
 ]
