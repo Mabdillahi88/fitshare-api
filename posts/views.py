9e6feb0a2c3c09812993239d479ead849aeb5d62
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-12-07 18:44:46 +0000

    Add search functionality to posts by username and title

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,28 @@
-from rest_framework import generics, permissions
-from profiles.permissions import IsOwnerOrReadOnly
+from rest_framework import generics, permissions, filters
+from django.db.models import Count
 from .models import Post
-from .serializers import PostSerializer, PostDetailSerializer
+from .serializers import PostSerializer
 
-# List and create posts
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
+
+
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.annotate(
+        comments_count=Count('comments', distinct=True),
+        likes_count=Count('likes', distinct=True)
+    ).order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['comments_count', 'likes_count', 'likes__created_at']
+    search_fields = ['owner__username', 'title']  # Search by author's username or post title
+
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.annotate(
+        comments_count=Count('comments', distinct=True),
+        likes_count=Count('likes', distinct=True)
+    ).order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-# Retrieve, update, and delete posts
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostDetailSerializer
-    permission_classes = [IsOwnerOrReadOnly]
