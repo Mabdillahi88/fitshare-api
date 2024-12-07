17914fe45d0af6ecfb54b23ac0db0ac077daadf1
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-12-07 15:06:45 +0000

    updating serializers for profile project

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,14 +1,26 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
-        fields = ['id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image', 'is_owner']
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 
+            'name', 'content', 'image', 'is_owner', 'following_id'
+        ]
 
     def get_is_owner(self, obj):
         request = self.context.get('request')
         return request and request.user == obj.owner
+
+    def get_following_id(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            follower = Follower.objects.filter(owner=request.user, followed=obj.owner).first()
+            return follower.id if follower else None
+        return None
