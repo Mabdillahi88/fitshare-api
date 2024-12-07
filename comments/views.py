a60960886fbd90c72e1064a756f757c64df50f3b
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-12-07 20:39:43 +0000

    Add filtering for comments by post

--- comments/views.py
+++ comments/views.py
@@ -2,15 +2,19 @@ from rest_framework import generics, permissions
 from profiles.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
-# List and create comments
+
 class CommentList(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]  # Add filter backend
+    filterset_fields = ['post']  # Filter by the related post
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
 
 # Retrieve, update, and delete comments
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
