21aff68c9196688fbead04e4d1732d40cde71007
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-12-07 09:45:25 +0000

    update to serializers in followers project

--- followers/serializers.py
+++ followers/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
+from django.db import IntegrityError
 from .models import Follower
 
 class FollowerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    followed = serializers.ReadOnlyField(source='followed.username')
+    followed_name = serializers.ReadOnlyField(source='followed.username')
 
     class Meta:
         model = Follower
-        fields = ['id', 'owner', 'followed', 'created_at']
+        fields = ['id', 'owner', 'followed', 'followed_name', 'created_at']
+
+    def create(self, validated_data):
+        """
+        Handles IntegrityError when a duplicate Follower is created.
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'You are already following this user.'
+            })
