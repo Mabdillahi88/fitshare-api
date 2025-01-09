f1b75185f1cd45e3488df1cc8354166d46f3706c
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2025-01-09 20:51:20 +0000

    changes to followers serializers

--- followers/serializers.py
+++ followers/serializers.py
@@ -6,13 +6,29 @@ from .models import Follower
 class FollowerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     followed_name = serializers.ReadOnlyField(source='followed.username')
-    followed = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all(), write_only=True)
+    followed = serializers.CharField(write_only=True)  # Accept either id or username as a string
 
     class Meta:
         model = Follower
         fields = ['id', 'owner', 'followed', 'followed_name', 'created_at']
 
+    def validate_followed(self, value):
+        """
+        Validate the 'followed' field to accept both ID and username.
+        """
+        try:
+            # Check if the value is a digit (indicating ID)
+            if value.isdigit():
+                return User.objects.get(id=value)
+            # Otherwise, treat it as a username
+            return User.objects.get(username=value)
+        except User.DoesNotExist:
+            raise ValidationError("The user you are trying to follow does not exist.")
+
     def create(self, validated_data):
+        """
+        Ensure the combination of 'owner' and 'followed' is unique.
+        """
         owner = self.context['request'].user
         followed = validated_data['followed']
 
