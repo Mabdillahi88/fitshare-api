98fb70958f3485d49400d07ccfe6a6d11d176a7f
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2025-03-26 09:54:24 +0300

    Add Achievement viewset for API endpoints

--- achievements/views.py
+++ achievements/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Achievement
+from .serializers import AchievementSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class AchievementViewSet(viewsets.ModelViewSet):
+    queryset = Achievement.objects.all()
+    serializer_class = AchievementSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
