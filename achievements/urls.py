eb7c694364be29dbaf50ecfb740cb36682f73a13
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2025-03-26 22:30:22 +0300

    Simplify achievements URL: use empty prefix so final route is /achievements/

--- achievements/urls.py
+++ achievements/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import AchievementViewSet
 
 router = DefaultRouter()
-router.register(r'achievements', AchievementViewSet, basename='achievement')
+# Register with an empty prefix so that the final route is simply /achievements/
+router.register(r'', AchievementViewSet, basename='achievement')
 
 urlpatterns = [
     path('', include(router.urls)),
