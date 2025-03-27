16ba49290d436a7b8f1ffc5aa68c3e28acd88662
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2025-03-27 21:17:34 +0300

    Add NotificationViewSet to expose notifications API

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Notification
+from .serializers import NotificationSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class NotificationViewSet(viewsets.ModelViewSet):
+    serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Only return notifications for the logged-in user
+        return Notification.objects.filter(recipient=self.request.user)
