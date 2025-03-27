d2d13eaf3ea0814619e04a5b541c4424479a5485
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2025-03-27 15:27:55 +0300

    Restrict Achievement viewset queryset to the logged-in user

--- achievements/views.py
+++ achievements/views.py
@@ -4,9 +4,11 @@ from .serializers import AchievementSerializer
 from rest_framework.permissions import IsAuthenticated
 
 class AchievementViewSet(viewsets.ModelViewSet):
-    queryset = Achievement.objects.all()
     serializer_class = AchievementSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Achievement.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
